0823556f015f3cab98e36b0b5c441c927a01d811
nur-987 <76473637+nur-987@users.noreply.github.com>
2021-10-07 00:38:07 +0800

    changes

---
+++ ListWithThread/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ListWithThread
+{
+    /// <summary>
+    /// Write a program which operate on a List and Does addition and Removal. 
+    /// It can be any List with any particular type. 
+    /// You have to spawn threads depending on user inputs 
+    /// which can add and remove from the original List.
+    /// </summary>
+    class Program
+    {
+        //not working. NO THREAD function yet
+
+        static void Main(string[] args)
+        {
+            Items item = new Items();
+            bool b = true;
+            while (b)
+            {
+                Console.WriteLine("Press 1) to ADD, 2) to REMOVE");
+                int input = Int32.Parse(Console.ReadLine());
+
+                if (input == 1)
+                {
+                    Console.WriteLine("Item NAME:");
+                    object tempName = Console.ReadLine();
+
+                    Items newItem = new Items();
+                    newItem.ItemName = tempName;
+
+                    Thread tdAdd = new Thread(new ParameterizedThreadStart(item.AddItem));
+                    tdAdd.Start(newItem);
+
+                }
+                else if (input == 2)
+                {
+                    Console.WriteLine("Name of item to remove: ");
+                    object tempName = Console.ReadLine();
+
+                    Items newItem = new Items();
+                    newItem.ItemName = tempName;
+
+                    Thread tdRemove = new Thread(new ParameterizedThreadStart(item.Remove));
+                    tdRemove.Start(newItem);
+                }
+                else
+                {
+                    Console.WriteLine("exiting program");
+                    b = false;
+                }
+            }
+            
+
+            Console.ReadLine();
+        }
+
+    }
+
+    class Items
+    {
+        public object ItemName { get; set; }
+
+        public static List<Items> ObjectItems = new List<Items>(); 
+        
+        public void AddItem(object objName)
+        {
+            Items newItem = (Items)objName;
+            ObjectItems.Add(newItem);
+            Console.WriteLine("Number of items in List: " + ObjectItems.Count);
+
+            Console.WriteLine("Items in list now:");
+            foreach (Items item in ObjectItems)
+            {
+                Console.WriteLine(item.ItemName);
+            }
+
+        }
+
+        public void Remove(object objName)
+        {
+            Items itemToRemove = (Items)objName;
+
+            foreach (Items item in ObjectItems)
+            {
+                if (item.ItemName == itemToRemove)
+                {
+                    ObjectItems.Remove(item);
+                    Console.WriteLine("remove successful");
+                }
+            }
+            Console.WriteLine("Number of items in List: " + ObjectItems.Count);
+        }
+    }
+    
+}
---
+++ ResumeRestartThread/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResumeRestartThread
+{
+    /// <summary>
+    /// Write a program which can spawn a thread which executes a method. 
+    /// The method takes a parameter called Resume or Restart. 
+    /// The method displays the numbers in a series. (1,2,3....). 
+    /// At any point of time You can spawn more than one thread. 
+    /// If the method is passed as Resume it will start the counter from the current counter value 
+    /// else it will start from 1 again.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public void Counter()
+        {
+
+        }
+
+    }
+   
+}
