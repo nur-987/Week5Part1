7afa7aa9fa72843200fa78d5ef7b62fc448b2152
nur-987 <76473637+nur-987@users.noreply.github.com>
2021-10-07 18:01:58 +0800

    new

--- ResumeRestartThread/Program.cs
+++ ResumeRestartThread/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ResumeRestartThread
@@ -13,16 +14,66 @@ namespace ResumeRestartThread
     /// At any point of time You can spawn more than one thread. 
     /// If the method is passed as Resume it will start the counter from the current counter value 
     /// else it will start from 1 again.
+    /// 
+    /// NOT COMPLETE => Resume
     /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
+            bool b = true;
+            while (b)
+            {
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("Resume - Restart - Exit");
+                string input = Console.ReadLine();
+
+                doSomthing(input);
+
+                if (input == "Exit")
+                {
+                    b = false;
+                }
+            }
+
+
+            
+          
         }
 
-        public void Counter()
+        public static void Counter()
         {
+            for (int i = 0; i < 10000; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(5000);
 
+                Resume(i);
+            }
+        }
+
+        public static void Resume (int x)
+        {
+            for (int i = x; i < 10000; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(5000);
+            }
+        }
+
+        public static void doSomthing(string input)
+        {
+            if (input == "Restart")
+            {
+                Thread threadRestart = new Thread(new ThreadStart(Counter));
+                threadRestart.Start();
+
+            }
+            if(input == "Resume")
+            {
+                Thread threadResume = new Thread(new ThreadStart(Counter));
+                threadResume.Start();
+            }
         }
 
     }
